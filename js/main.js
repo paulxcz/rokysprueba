98dedf777d5888de59b04a2d534554ce6258314b
paul <paulxcz@gmail.com>
2021-02-06 19:58:42 -0500

    mi primer commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+    $('.flexslider').flexslider({
+
+        prevText: "",
+        nextText: "",
+        pauseOnAction: false,
+        slideshowSpeed: 3000,
+        animation: "slide",
+    })
+});
+
+$(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:false,
+        prevHtml: '',
+        slideMargin: 37,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  });
+
+  $(document).ready(function() {
+    $('#autoWidth2').lightSlider({
+        autoWidth:true,
+        loop:false,
+        slideMargin: 30,
+        onSliderLoad: function() {
+            $('#autoWidth2').removeClass('cS-hidden');
+        } 
+    });  
+  });
